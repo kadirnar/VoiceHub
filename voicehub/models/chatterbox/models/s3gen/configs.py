b99dba08b4060f3751885ce9edda0301e3424c27
kadirnar <kadir.nar@hotmail.com>
2025-06-27 17:50:53 +0000

    🌠 Add Chatterbox model support

---
+++ voicehub/models/chatterbox/models/s3gen/configs.py
@@ -0,0 +1,10 @@
+from voicehub.models.s3gen.utils import AttrDict
+
+CFM_PARAMS = AttrDict({
+    "sigma_min": 1e-06,
+    "solver": "euler",
+    "t_scheduler": "cosine",
+    "training_cfg_rate": 0.2,
+    "inference_cfg_rate": 0.7,
+    "reg_loss_type": "l1"
+})
